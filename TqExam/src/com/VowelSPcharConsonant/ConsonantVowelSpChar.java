ada835ef0d8d5ea263974988f0ec3f594fb87a94
Pranav <pranavkumbhar4127@gmail.com>
2023-04-10 14:54:36 +0530

    tqexam

---
+++ TqExam/src/com/VowelSPcharConsonant/ConsonantVowelSpChar.java
@@ -0,0 +1,38 @@
+package com.VowelSPcharConsonant;
+
+import java.util.Scanner;
+
+public class ConsonantVowelSpChar {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Character:");
+		char ch= sc.next().charAt(0);
+		
+		
+		
+		if (((ch>=65)&&(ch<=90))||(ch>=97)&&(ch<=122))
+		{
+			if((ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U')||(ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+			{
+				System.out.println("Vowel");
+			}
+			else
+			{
+				System.out.println("Consonant");
+			}
+			
+		}
+		else if ((ch>=48)&&(ch<=57))
+			
+			{
+				System.out.println("Digit");
+			}
+		else 
+		{
+			System.out.println("Special Characcter");
+		}
+		
+	}
+
+}
